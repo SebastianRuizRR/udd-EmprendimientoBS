67b288d14e9c615d4514cbbf1372b00e9d7b6c6b
Sebastian Ruiz <sebastianruizriffo@gmail.com>
2025-11-30 02:43:07 +0000

    Actualizaciones proyecto

--- web/src/vite-env.d.ts
+++ web/src/vite-env.d.ts
@@ -1,1 +1,9 @@
+// web/src/vite-env.d.ts
+
 /// <reference types="vite/client" />
+
+// Añadimos estas declaraciones explícitas para que TypeScript reconozca los assets.
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.png";
+declare module "*.svg";
